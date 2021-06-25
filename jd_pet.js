72a262313916db54036a6518c9a254c582ba64f4
FearNoManButGod <85055501+FearNoManButGod@users.noreply.github.com>
2021-06-25 08:51:45 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -517,8 +517,8 @@ function shareCodesFormat() {
       });
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
-      const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
-      newShareCodes = shareCodes[tempIndex].split('@');
+//       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
+//       newShareCodes = shareCodes[tempIndex].split('@');
     }
     //因好友助力功能下线。故暂时屏蔽
  /*    const readShareCodeRes = await readShareCode();
